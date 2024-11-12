6d5a7261c4ddc3b8eb0eecec6622bcf26235dbaf
MphoNdlela16 <161179056+MphoCodes@users.noreply.github.com>
2024-11-12 12:38:42 +0200

    Update App.test.js

--- Customer_portal/Customer_portal/frontend/src/App.test.js
+++ Customer_portal/Customer_portal/frontend/src/App.test.js
@@ -4,15 +4,12 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
 
 const MockApp = () => {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   );
 };
 
