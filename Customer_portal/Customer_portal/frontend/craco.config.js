bab591f394e882a7cd4b9b93aedeb5a6331b3dc8
MphoNdlela16 <161179056+MphoCodes@users.noreply.github.com>
2024-11-12 11:42:23 +0200

    Update craco.config.js

--- Customer_portal/Customer_portal/frontend/craco.config.js
+++ Customer_portal/Customer_portal/frontend/craco.config.js
@@ -1,23 +1,28 @@
+// craco.config.js
 const path = require('path');
 
 module.exports = {
+  babel: {
+    plugins: ['@babel/plugin-proposal-private-property-in-object'],
+  },
   webpack: {
     alias: {},
-    plugins: [],
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        "https": require.resolve("https-browserify"),
-        "http": require.resolve("stream-http"),
-        "url": require.resolve("url"),
-        "buffer": require.resolve("buffer"),
-        "stream": require.resolve("stream-browserify"),
-        "crypto": require.resolve("crypto-browserify"), 
+        https: require.resolve('https-browserify'),
+        http: require.resolve('stream-http'),
+        url: require.resolve('url'),
+        buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
+        crypto: require.resolve('crypto-browserify'),
       };
       return webpackConfig;
     },
   },
+  jest: {
+    configure: {
+      transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
+    },
+  },
 };
-
-
-
